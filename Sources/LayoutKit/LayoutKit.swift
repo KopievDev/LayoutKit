56ddac6523983dd4a8a32c43cbaa8441a3c4f820
Kopiev Ivan <x@mustdev.ru>
2023-09-25 17:11:50 +0300

    Init

--- Sources/LayoutKit/LayoutKit.swift
+++ Sources/LayoutKit/LayoutKit.swift
@@ -1,6 +1,323 @@
-public struct LayoutKit {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public struct LayoutKit {
+    var view: UIView
+    public init(view: UIView) {
+        self.view = view
     }
+}
+
+public extension LayoutKit {
+    enum Relation {
+        case equal
+        case lessOrEqual
+        case greaterOrEqual
+    }
+
+    indirect enum Edge {
+        case top(CGFloat)
+        case left(CGFloat)
+        case right(CGFloat)
+        case bottom(CGFloat)
+        case centerX(CGFloat)
+        case centerY(CGFloat)
+        case safeTop(CGFloat)
+        case safeBottom(CGFloat)
+        case spaceH(CGFloat)
+        case spaceV(CGFloat)
+        case padding(CGFloat)
+        case safePadding(CGFloat)
+
+        case multiplier(CGFloat, edge: Self)
+        case relation(Relation, edge: Self)
+        case priority(CGFloat, edge: Self)
+
+        public static let top = Self.top(0)
+        public static var left = Self.left(0)
+        public static var right = Self.right(0)
+        public static var bottom  = Self.bottom(0)
+        public static var centerX  = Self.centerX(0)
+        public static var centerY  = Self.centerY(0)
+        public static var safeTop = Self.safeTop(0)
+        public static var safeBottom = Self.safeBottom(0)
+        public static var padding = Self.padding(0)
+        public static var safePadding = Self.safePadding(0)
+
+    }
+
+    @discardableResult
+    func pinTo(_ view: UIView, edges: Edge...) -> Self {
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        edges.forEach { edge in pinTo(view, edge: edge) }
+        return self
+    }
+
+    @discardableResult
+    func pinTo(_ view: UIView, edges: [Edge]) -> Self {
+        edges.forEach { edge in pinTo(view, edge: edge) }
+        return self
+    }
+
+
+    private func pinTo(_ view: UIView, edge: Edge) {
+        let mainView = self.view
+        switch edge {
+        case .top(let constant):
+            mainView.topAnchor.constraint(equalTo: view.topAnchor, constant: constant).isActive = true
+        case .left(let constant):
+            mainView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant).isActive = true
+        case .right(let constant):
+            mainView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant).isActive = true
+        case .bottom(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant).isActive = true
+        case .centerX(let constant):
+            mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: constant).isActive = true
+        case .centerY(let constant):
+            mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+        case .spaceH(let constant):
+            mainView.rightAnchor.constraint(equalTo: view.leftAnchor, constant: -constant).isActive = true
+        case .spaceV(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: -constant).isActive = true
+        case .multiplier(let multiplier, edge: let edge):
+            pinTo(view, edge: edge, multiplier: multiplier)
+        case .relation(let relation, edge: let edge):
+            pinTo(view, edge: edge, relation: relation)
+        case .priority(let priority, edge: let edge):
+            pinTo(view, edge: edge, priority: priority)
+        case .safeBottom(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -constant).isActive = true
+        case .safeTop(let constant):
+            mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).isActive = true
+        case .padding(let constant):
+            pinTo(view, edges: .top(constant), .left(constant), .right(constant), .bottom(constant))
+        case .safePadding(let constant):
+            pinTo(view, edges: .safeTop(constant), .left(constant), .right(constant), .safeBottom(constant))
+        }
+    }
+
+    private func pinTo(_ view: UIView, edge: Edge, multiplier: CGFloat) {
+        let mainView = self.view
+        switch edge {
+        case .top:
+            mainView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: multiplier).isActive = true
+        case .left:
+            mainView.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: multiplier).isActive = true
+        case .right:
+            mainView.rightAnchor.constraint(equalToSystemSpacingAfter: view.rightAnchor, multiplier: multiplier).isActive = true
+        case .bottom:
+            mainView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: multiplier).isActive = true
+        case .centerX:
+            mainView.centerXAnchor.constraint(equalToSystemSpacingAfter: view.centerXAnchor, multiplier: multiplier).isActive = true
+        case .centerY:
+            mainView.centerYAnchor.constraint(equalToSystemSpacingBelow: view.centerYAnchor, multiplier: multiplier).isActive = true
+        case .spaceH:
+            mainView.rightAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: multiplier).isActive = true
+        case .spaceV:
+            mainView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: multiplier).isActive = true
+        case .multiplier(_, edge: _):
+            fatalError("Can't be multiplier with multiplier")
+        case .relation(_, edge: _):
+            fatalError("Can't be relation with relation")
+        case .priority(_, edge: _):
+            fatalError("Didn't implemented priority attribute")
+        case .safeTop:
+            mainView.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: multiplier).isActive = true
+        case .safeBottom:
+            mainView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: multiplier).isActive = true
+        case .padding:
+            mainView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: multiplier).isActive = true
+            mainView.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: multiplier).isActive = true
+            mainView.rightAnchor.constraint(equalToSystemSpacingAfter: view.rightAnchor, multiplier: multiplier).isActive = true
+            mainView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: multiplier).isActive = true
+        case .safePadding:
+            mainView.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: multiplier).isActive = true
+            mainView.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: multiplier).isActive = true
+            mainView.rightAnchor.constraint(equalToSystemSpacingAfter: view.rightAnchor, multiplier: multiplier).isActive = true
+            mainView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: multiplier).isActive = true
+
+        }
+    }
+
+    private func pinTo(_ view: UIView, edge: Edge, priority: CGFloat) {
+        let mainView = self.view
+        switch edge {
+        case .top(let constant):
+            mainView.topAnchor.constraint(equalTo: view.topAnchor, constant: constant).priority(priority).isActive = true
+        case .left(let constant):
+            mainView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant).priority(priority).isActive = true
+        case .right(let constant):
+            mainView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant).priority(priority).isActive = true
+        case .bottom(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant).priority(priority).isActive = true
+        case .centerX(let constant):
+            mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: constant).priority(priority).isActive = true
+        case .centerY(let constant):
+            mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).priority(priority).isActive = true
+        case .spaceH(let constant):
+            mainView.rightAnchor.constraint(equalTo: view.leftAnchor, constant: -constant).priority(priority).isActive = true
+        case .spaceV(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: -constant).priority(priority).isActive = true
+        case .multiplier(_, edge: _):
+            fatalError("Didn't implemented multipier attribute")
+        case .relation(_, edge: _):
+            fatalError("Didn't implemented relation attribute")
+        case .priority(_, edge: _):
+            fatalError("Didn't implemented priority attribute")
+        case .safeBottom(let constant):
+            mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -constant).priority(priority).isActive = true
+        case .safeTop(let constant):
+            mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).priority(priority).isActive = true
+        case .padding(let constant):
+            mainView.topAnchor.constraint(equalTo: view.topAnchor, constant: constant).priority(priority).isActive = true
+            mainView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant).priority(priority).isActive = true
+            mainView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant).priority(priority).isActive = true
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant).priority(priority).isActive = true
+        case .safePadding(let constant):
+            mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).priority(priority).isActive = true
+            mainView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant).priority(priority).isActive = true
+            mainView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant).priority(priority).isActive = true
+            mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -constant).priority(priority).isActive = true
+        }
+    }
+
+    private func pinTo(_ view: UIView, edge: Edge, relation: Relation) {
+        let mainView = self.view
+        switch relation {
+        case .equal:
+            fatalError("Use without relation")
+        case .lessOrEqual:
+            switch edge {
+            case .top(let constant):
+                mainView.topAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: constant).isActive = true
+            case .left(let constant):
+                mainView.leftAnchor.constraint(lessThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+            case .right(let constant):
+                mainView.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+            case .bottom(let constant):
+                mainView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -constant).isActive = true
+            case .centerX(let constant):
+                mainView.centerXAnchor.constraint(lessThanOrEqualTo: view.centerXAnchor, constant: constant).isActive = true
+            case .centerY(let constant):
+                mainView.centerYAnchor.constraint(lessThanOrEqualTo: view.centerYAnchor, constant: constant).isActive = true
+            case .spaceH(let constant):
+                mainView.rightAnchor.constraint(lessThanOrEqualTo: view.leftAnchor, constant: -constant).isActive = true
+            case .spaceV(let constant):
+                mainView.bottomAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: -constant).isActive = true
+            case .multiplier(_, edge: _):
+                fatalError("Can't be multiplier with relation")
+            case .relation(_, edge: _):
+                fatalError("Can't be relation with relation")
+            case .priority(_, edge: _):
+                fatalError("Didn't implemented priority attribute")
+            case .safeTop(let constant):
+                mainView.topAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
+                                     constant: constant).isActive = true
+            case .safeBottom(let constant):
+                mainView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                        constant: constant).isActive = true
+            case .padding(let constant):
+                mainView.topAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: constant).isActive = true
+                mainView.leftAnchor.constraint(lessThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+                mainView.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+                mainView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -constant).isActive = true
+            case .safePadding(let constant):
+                mainView.topAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).isActive = true
+                mainView.leftAnchor.constraint(lessThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+                mainView.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+                mainView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -constant).isActive = true
+            }
+        case .greaterOrEqual:
+            switch edge {
+            case .top(let constant):
+                mainView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: constant).isActive = true
+            case .left(let constant):
+                mainView.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+            case .right(let constant):
+                mainView.rightAnchor.constraint(greaterThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+            case .bottom(let constant):
+                mainView.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: -constant).isActive = true
+            case .centerX(let constant):
+                mainView.centerXAnchor.constraint(greaterThanOrEqualTo: view.centerXAnchor, constant: constant).isActive = true
+            case .centerY(let constant):
+                mainView.centerYAnchor.constraint(greaterThanOrEqualTo: view.centerYAnchor, constant: constant).isActive = true
+            case .spaceH(let constant):
+                mainView.rightAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: -constant)
+                    .isActive = true
+            case .spaceV(let constant):
+                mainView.bottomAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: -constant)
+                    .isActive = true
+            case .multiplier(_, edge: _):
+                fatalError("Can't be multiplier with relation")
+            case .relation(_, edge: _):
+                fatalError("Can't be relation with relation")
+            case .priority(_, edge: _):
+                fatalError("Didn't implemented priority attribute")
+            case .safeTop(let constant):
+                mainView.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
+                                     constant: constant).isActive = true
+            case .safeBottom(let constant):
+                mainView.bottomAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                        constant: constant).isActive = true
+            case .padding(let constant):
+                mainView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: constant).isActive = true
+                mainView.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+                mainView.rightAnchor.constraint(greaterThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+                mainView.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: -constant).isActive = true
+            case .safePadding(let constant):
+                mainView.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).isActive = true
+                mainView.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: constant).isActive = true
+                mainView.rightAnchor.constraint(greaterThanOrEqualTo: view.rightAnchor, constant: -constant).isActive = true
+                mainView.bottomAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -constant).isActive = true
+
+            }
+        }
+    }
+
+    enum LayoutDimension { case width, height }
+
+    @discardableResult
+    func width(_ view: UIView, equalTo size: LayoutDimension, multiplier: CGFloat = 1, constant: CGFloat = 0) -> Self  {
+        let mainView = self.view
+        switch size {
+        case .width:
+            mainView.widthAnchor.constraint(equalTo: view.widthAnchor,
+                                   multiplier: multiplier,
+                                   constant: constant).isActive = true
+        case .height:
+            mainView.widthAnchor.constraint(equalTo: view.heightAnchor,
+                                   multiplier: multiplier,
+                                   constant: constant).isActive = true
+        }
+        return self
+    }
+
+    @discardableResult
+    func height(_ view: UIView, equalTo size: LayoutDimension, multiplier: CGFloat = 1, constant: CGFloat = 0) -> Self  {
+        let mainView = self.view
+        switch size {
+        case .width:
+            mainView.heightAnchor.constraint(equalTo: view.widthAnchor,
+                                    multiplier: multiplier,
+                                    constant: constant).isActive = true
+        case .height:
+            mainView.heightAnchor.constraint(equalTo: view.heightAnchor,
+                                    multiplier: multiplier,
+                                    constant: constant).isActive = true
+        }
+        return self
+    }
+
+}
+
+public extension UIView {
+    var layout: LayoutKit { LayoutKit(view: self) }
+}
+
+public extension NSLayoutConstraint {
+    @discardableResult
+    func priority(_ priority: CGFloat) -> Self {
+        self.priority = UILayoutPriority(Float(priority))
+        return self
+    }
+
 }
